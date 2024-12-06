c26f5f5ea6155d53965a4c3ddbb88be48a9ed0d0
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 21:12:14 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -1,8 +1,25 @@
-public static Matrix3 CreateTranslationMatrix(float x, float y)
+#version 330 core
+
+uniform vec2 ViewportSize;
+uniform mat3 TransformMatrix;      // Добавлено
+uniform mat3 TranslationMatrix;    // Добавлено
+
+layout (location = 0) in vec2 aPosition;
+layout (location = 1) in vec4 aColor;
+
+out vec4 vColor;
+
+void main()
 {
-    return new Matrix3(
-        1f, 0f, 0f, // Первый столбец
-        0f, 1f, 0f, // Второй столбец
-        x,  y,  1f  // Третий столбец
-    );
+    // Преобразуем положение вершины через матрицы TransformMatrix и TranslationMatrix
+    vec3 position = vec3(aPosition, 1.0);
+    position = TranslationMatrix * TransformMatrix * position;
+
+    // Преобразуем координаты в нормализованное устройство
+    float nx = position.x / ViewportSize.x * 2.0 - 1.0;
+    float ny = position.y / ViewportSize.y * 2.0 - 1.0;
+    gl_Position = vec4(nx, ny, 0.0, 1.0);
+
+    // Цвет с возможным фактором умножения
+    vColor = aColor;
 }
