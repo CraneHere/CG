7c1ecb1e68043e9d28f6f9bbe39a12bc9c43ebcf
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 21:22:09 +0300

    d

--- f.cs
+++
@@ -1,5 +0,0 @@
-if (!Shader.CompileVertexShader(vertexShaderCode, out this.VertexShaderHandle, out string vertexShaderCompileError))
-{
-    Console.WriteLine("Vertex shader compilation error: " + vertexShaderCompileError);
-    throw new ArgumentException(vertexShaderCompileError);
-}
