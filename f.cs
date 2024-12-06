327576927b1388a7b7d5b96ffb2fd09ecf2ce669
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 20:56:08 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -1,32 +1,8 @@
-protected override void OnUpdateFrame(FrameEventArgs args)
+public static Matrix3 CreateTranslationMatrix(float x, float y)
 {
-    this.animationTime += this.animationSpeed;
-    if (this.animationTime >= 1f)
-    {
-        this.animationTime = 0f;
-
-        // Смена начальной и целевой позиции
-        var tempShape = this.initialShape;
-        this.initialShape = this.targetShape;
-        this.targetShape = tempShape;
-
-        var tempOffset = this.positionOffset;
-        this.positionOffset = this.targetOffset;
-        this.targetOffset = tempOffset;
-    }
-
-    // Линейная интерполяция формы
-    for (int i = 0; i < this.currentShape.Length; i++)
-    {
-        this.currentShape[i] = Vector2.Lerp(this.initialShape[i], this.targetShape[i], this.animationTime);
-    }
-
-    // Линейная интерполяция позиции
-    var interpolatedOffset = Vector2.Lerp(this.positionOffset, this.targetOffset, this.animationTime);
-
-    // Создание трансляционной матрицы
-    var translationMatrix = CreateTranslationMatrix(interpolatedOffset.X, interpolatedOffset.Y);
-
-    // Передача матрицы в шейдер
-    this.shaderProgram.SetUniform("TranslationMatrix", translationMatrix);
+    return new Matrix3(
+        1f, 0f, 0f, // Первый столбец
+        0f, 1f, 0f, // Второй столбец
+        x,  y,  1f  // Третий столбец
+    );
 }
