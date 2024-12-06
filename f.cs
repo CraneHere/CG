40def17a9ea2e5a9913465ffe6477c66e379db01
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 21:15:05 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -1,25 +1,5 @@
-#version 330 core
-
-uniform vec2 ViewportSize;
-uniform mat3 TransformMatrix;      // Добавлено
-uniform mat3 TranslationMatrix;    // Добавлено
-
-layout (location = 0) in vec2 aPosition;
-layout (location = 1) in vec4 aColor;
-
-out vec4 vColor;
-
-void main()
+if (!Shader.CompileVertexShader(vertexShaderCode, out this.VertexShaderHandle, out string vertexShaderCompileError))
 {
-    // Преобразуем положение вершины через матрицы TransformMatrix и TranslationMatrix
-    vec3 position = vec3(aPosition, 1.0);
-    position = TranslationMatrix * TransformMatrix * position;
-
-    // Преобразуем координаты в нормализованное устройство
-    float nx = position.x / ViewportSize.x * 2.0 - 1.0;
-    float ny = position.y / ViewportSize.y * 2.0 - 1.0;
-    gl_Position = vec4(nx, ny, 0.0, 1.0);
-
-    // Цвет с возможным фактором умножения
-    vColor = aColor;
+    Console.WriteLine("Vertex shader compilation error: " + vertexShaderCompileError);
+    throw new ArgumentException(vertexShaderCompileError);
 }
